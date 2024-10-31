c32657a1041e4888e90b5d2d1044ecb684ca23b3
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-10-31 15:50:35 -0400

    chore: swap yarn for npm in workflows

--- packages/ai-chat/src/components/extended-button/extended-button.ts
+++ packages/ai-chat/src/components/extended-button/extended-button.ts
@@ -14,6 +14,7 @@ import prefix from "../../globals/settings.js";
 /**
  * Component extending the @carbon/web-components' button
  */
+//@ts-ignore
 @customElement(`${prefix}-extended-button`)
 class ExtendedButton extends extendedButton {}
 
