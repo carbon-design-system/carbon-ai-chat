0c8d62cd2799e70ca4ac086e84412679a1575ff0
ewinters@us.ibm.com <ewinters@us.ibm.com>
2025-08-11 16:37:58 -0400

    chore: fix tests

--- demo/tests/smoke.spec.ts
+++ demo/tests/smoke.spec.ts
@@ -28,7 +28,7 @@ test("smoke React", async ({ page }) => {
     .getByTestId(makeTestId(PageObjectId.INPUT_SEND, OverlayPanelName.MAIN))
     .click();
   await expect(page.locator("#WAC__message-3")).toContainText(
-    'Lorem bold text italics text ipsum odor amet, consectetuer adipiscing elit. alert("inline code"); aliquet non platea elementum morbi porta accumsan. Tortor libero consectetur dapibus volutpat porta vestibulum.'
+    'Carbon is a'
   );
   await close.click();
 });
@@ -62,7 +62,7 @@ test("smoke web component", async ({ page }) => {
     .getByTestId(makeTestId(PageObjectId.INPUT_SEND, OverlayPanelName.MAIN))
     .click();
   await expect(page.locator("#WAC__message-3")).toContainText(
-    'Lorem bold text italics text ipsum odor amet, consectetuer adipiscing elit. alert("inline code"); aliquet non platea elementum morbi porta accumsan. Tortor libero consectetur dapibus volutpat porta vestibulum.'
+    'Carbon is a'
   );
   await close.click();
 });
