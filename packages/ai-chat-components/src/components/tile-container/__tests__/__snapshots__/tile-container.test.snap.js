96a470b94c5bdbd89b51c859437876ee3dfa78a5
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-08-29 15:12:35 +0530

    update snapshot

--- packages/ai-chat-components/src/components/tile-container/__tests__/__snapshots__/tile-container.test.snap.js
+++ packages/ai-chat-components/src/components/tile-container/__tests__/__snapshots__/tile-container.test.snap.js
@@ -1,16 +1,10 @@
-/*
- *  Copyright IBM Corp. 2025
- *
- *  This source code is licensed under the Apache-2.0 license found in the
- *  LICENSE file in the root directory of this source tree.
- *
- *  @license
- */
-
+/* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["should render cds-aichat-tile-container in DOM"] =
-  `<cds-aichat-tile-container>
-    tile
-  </cds-aichat-tile-container>`;
-/* end snapshot extended-tile should render with cds-tile minimum attributes */
+snapshots["aichat tile should render cds-aichat-tile-container in DOM"] = 
+`<cds-aichat-tile-container>
+  tile
+</cds-aichat-tile-container>
+`;
+/* end snapshot aichat tile should render cds-aichat-tile-container in DOM */
+
