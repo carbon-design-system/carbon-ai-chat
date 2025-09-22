6c55fbde52b8535db3a35e243e53963f3b89caed
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-09-22 19:00:06 +0530

    chore: update snapshots

--- packages/ai-chat-components/src/components/extended-button/__tests__/__snapshots__/extended-button.test.snap.js
+++ packages/ai-chat-components/src/components/extended-button/__tests__/__snapshots__/extended-button.test.snap.js
@@ -1,8 +1,8 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["extended-button should render with cds-button minimum attributes"] =
-  `<cds-aichat-extended-button
+snapshots["extended-button should render with cds-button minimum attributes"] = 
+`<cds-aichat-extended-button
   has-main-content=""
   kind="primary"
   size="lg"
@@ -15,3 +15,4 @@ snapshots["extended-button should render with cds-button minimum attributes"] =
 </cds-aichat-extended-button>
 `;
 /* end snapshot extended-button should render with cds-button minimum attributes */
+
--- packages/ai-chat-components/src/components/tile-container/__tests__/__snapshots__/tile-container.test.snap.js
+++ packages/ai-chat-components/src/components/tile-container/__tests__/__snapshots__/tile-container.test.snap.js
@@ -1,9 +1,10 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["aichat tile should render cds-aichat-tile-container in DOM"] =
-  `<cds-aichat-tile-container>
+snapshots["aichat tile should render cds-aichat-tile-container in DOM"] = 
+`<cds-aichat-tile-container>
   tile
 </cds-aichat-tile-container>
 `;
 /* end snapshot aichat tile should render cds-aichat-tile-container in DOM */
+
