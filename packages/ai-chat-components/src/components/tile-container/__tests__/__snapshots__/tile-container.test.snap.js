b87cd62133fdbd16c277c67119493d05efb24432
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-09-22 18:10:16 +0530

    chore: delete snapshots

--- packages/ai-chat-components/src/components/tile-container/__tests__/__snapshots__/tile-container.test.snap.js
+++
@@ -1,9 +0,0 @@
-/* @web/test-runner snapshot v1 */
-export const snapshots = {};
-
-snapshots["aichat tile should render cds-aichat-tile-container in DOM"] =
-  `<cds-aichat-tile-container>
-  tile
-</cds-aichat-tile-container>
-`;
-/* end snapshot aichat tile should render cds-aichat-tile-container in DOM */
