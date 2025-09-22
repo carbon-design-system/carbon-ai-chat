19717a1df57ceaa372bdd653209820e5f83d6fc7
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-09-22 14:38:12 +0530

    chore: citation card update

--- packages/ai-chat/src/chat/legacy/components/responseTypes/card/CardItemComponent.tsx
+++ packages/ai-chat/src/chat/legacy/components/responseTypes/card/CardItemComponent.tsx
@@ -7,13 +7,21 @@
  *  @license
  */
 
+import TileContainer from "@carbon/ai-chat-components/es/react/tile-container.js";
 import Tile from "../../../../components/carbon/Tile";
 import cx from "classnames";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectInputState } from "../../../../store/selectors";
+import { AppState } from "../../../../../types/state/AppState";
+import { THROW_ERROR } from "../../../../utils/constants";
+import { useServiceManager } from "../../../../hooks/useServiceManager";
 
 import { HasRequestFocus } from "../../../../../types/utilities/HasRequestFocus";
 import { LocalMessageItem } from "../../../../../types/messaging/LocalMessageItem";
-import { BodyWithFooterComponent } from "../../BodyWithFooterComponent";
+import { BodyMessageComponents } from "../util/BodyMessageComponents";
+import { FooterButtonComponents } from "../util/FooterButtonComponents";
+import { useLanguagePack } from "../../../../hooks/useLanguagePack";
 import {
   CardItem,
   MessageResponse,
@@ -46,23 +54,55 @@ interface CardItemComponentProps extends HasRequestFocus {
  */
 function CardItemComponent(props: CardItemComponentProps) {
   const { ignoreMaxWidth } = props;
+  const languagePack = useLanguagePack();
+  const inputState = useSelector(selectInputState);
+  const appConfig = useSelector((state: AppState) => state.config);
+  const serviceManager = useServiceManager();
   const item = props.localMessageItem.item as CardItem;
   return (
-    <Tile
-      className={cx("cds-aichat--card-message-component", {
-        "cds-aichat--max-width-small":
-          !ignoreMaxWidth && item.max_width === WidthOptions.SMALL,
-        "cds-aichat--max-width-medium":
-          !ignoreMaxWidth && item.max_width === WidthOptions.MEDIUM,
-        "cds-aichat--max-width-large":
-          !ignoreMaxWidth && item.max_width === WidthOptions.LARGE,
-      })}
-    >
-      <BodyWithFooterComponent
-        {...props}
+    <TileContainer>
+      <Tile
+        className={cx("cds-aichat--card-message-component", {
+          "cds-aichat--max-width-small":
+            !ignoreMaxWidth && item.max_width === WidthOptions.SMALL,
+          "cds-aichat--max-width-medium":
+            !ignoreMaxWidth && item.max_width === WidthOptions.MEDIUM,
+          "cds-aichat--max-width-large":
+            !ignoreMaxWidth && item.max_width === WidthOptions.LARGE,
+        })}
+      >
+        <BodyMessageComponents
+          message={props.localMessageItem}
+          originalMessage={props.fullMessage}
+          languagePack={languagePack}
+          requestInputFocus={props.requestFocus}
+          disableUserInputs={inputState.isReadonly}
+          config={appConfig}
+          isMessageForInput={props.isMessageForInput}
+          scrollElementIntoView={THROW_ERROR}
+          serviceManager={serviceManager}
+          hideFeedback
+          showChainOfThought={false}
+          allowNewFeedback={false}
+          renderMessageComponent={props.renderMessageComponent}
+        />
+      </Tile>
+      <FooterButtonComponents
+        message={props.localMessageItem}
+        originalMessage={props.fullMessage}
+        languagePack={languagePack}
+        requestInputFocus={props.requestFocus}
+        disableUserInputs={inputState.isReadonly}
+        config={appConfig}
+        isMessageForInput={props.isMessageForInput}
+        scrollElementIntoView={THROW_ERROR}
+        serviceManager={serviceManager}
+        hideFeedback
+        showChainOfThought={false}
+        allowNewFeedback={false}
         renderMessageComponent={props.renderMessageComponent}
       />
-    </Tile>
+    </TileContainer>
   );
 }
 
