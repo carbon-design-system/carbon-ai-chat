10a6c0c5911bfa3598bd0c278719c435888ae321
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-09-22 16:18:50 +0530

    fix: snapshot

--- packages/ai-chat-components/src/components/extended-button/__tests__/__snapshots__/extended-button.test.snap.js
+++ packages/ai-chat-components/src/components/extended-button/__tests__/__snapshots__/extended-button.test.snap.js
@@ -1,10 +1,4 @@
-/*
- *  Copyright IBM Corp. 2025
- *
- *  This source code is licensed under the Apache-2.0 license found in the
- *  LICENSE file in the root directory of this source tree.
- */
-
+/* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
 snapshots["extended-button should render with cds-button minimum attributes"] =
