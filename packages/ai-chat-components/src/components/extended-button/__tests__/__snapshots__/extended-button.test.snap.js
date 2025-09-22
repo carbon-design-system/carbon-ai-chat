46a448c816fb300ea3ea2e83f29f597ed5521871
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-09-22 18:13:44 +0530

    chore: update snapshots

---
+++ packages/ai-chat-components/src/components/extended-button/__tests__/__snapshots__/extended-button.test.snap.js
@@ -0,0 +1,17 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["extended-button should render with cds-button minimum attributes"] =
+  `<cds-aichat-extended-button
+  has-main-content=""
+  kind="primary"
+  size="lg"
+  tab-index="0"
+  tooltip-alignment=""
+  tooltip-position="top"
+  type="button"
+>
+  button
+</cds-aichat-extended-button>
+`;
+/* end snapshot extended-button should render with cds-button minimum attributes */
