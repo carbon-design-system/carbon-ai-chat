0a604314a4aae86bebad2d876324f1db7f80610b
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2025-09-25 15:13:17 +0530

    chore: revert import path to test

--- packages/ai-chat-components/src/components/tile-container/__tests__/tile-container.test.ts
+++ packages/ai-chat-components/src/components/tile-container/__tests__/tile-container.test.ts
@@ -9,7 +9,8 @@
  */
 
 import { html, fixture, expect } from "@open-wc/testing";
-import "@carbon/ai-chat-components/es/components/tile-container/index.js";
+// import "@carbon/ai-chat-components/es/components/tile-container/index.js";
+import "../../../../es/components/tile-container/src/tile-container.js";
 import "@carbon/web-components/es/components/tile/tile.js";
 import CDSAIChatTileContainer from "../src/tile-container.js";
 
