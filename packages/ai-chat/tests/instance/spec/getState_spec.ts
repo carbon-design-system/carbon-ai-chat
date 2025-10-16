ethanwinters/carbon-ai-chat-fork

feat: add a "change:state" event
This will allow folks to watch internal chat state changes

packages/ai-chat/tests/instance/spec/getState_spec.ts
@@ -13,6 +13,8 @@ import {
   setupAfterEach,
   setupBeforeEach,
 } from "../helpers/chatInstanceTestHelpers";
+import { BusEventType } from "../../../src/types/events/eventBusTypes";
+import { ViewType } from "../../../src/types/instance/apiTypes";
 
 describe("ChatInstance.getState", () => {
   beforeEach(setupBeforeEach);
@@ -54,5 +56,93 @@ describe("ChatInstance.getState", () => {
     expect(() => {
       (state.humanAgent as { isConnecting: boolean }).isConnecting = true;
     }).toThrow();
+  });
+
+  it("fires STATE_CHANGE event when state changes", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    const stateChangeEvents: any[] = [];
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvents.push(event);
+      },
+    });
+
+    // Change the view state which should trigger STATE_CHANGE
+    await instance.changeView("mainWindow" as ViewType);
+
+    // Should have received at least one STATE_CHANGE event
+    expect(stateChangeEvents.length).toBeGreaterThan(0);
+
+    const lastEvent = stateChangeEvents[stateChangeEvents.length - 1];
+    expect(lastEvent.type).toBe(BusEventType.STATE_CHANGE);
+    expect(lastEvent.previousState).toBeDefined();
+    expect(lastEvent.newState).toBeDefined();
+    expect(lastEvent.newState.viewState.mainWindow).toBe(true);
+  });
+
+  it("provides both previous and new state in STATE_CHANGE event", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    let stateChangeEvent: any = null;
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvent = event;
+      },
+    });
+
+    // Change the unread indicator
+    instance.updateBotUnreadIndicatorVisibility(true);
+
+    // Should have received the event
+    expect(stateChangeEvent).not.toBeNull();
+    expect(stateChangeEvent.previousState.showUnreadIndicator).toBe(false);
+    expect(stateChangeEvent.newState.showUnreadIndicator).toBe(true);
+  });
+
+  it("does not fire STATE_CHANGE event when state does not change", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    const stateChangeEvents: any[] = [];
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvents.push(event);
+      },
+    });
+
+    const initialCount = stateChangeEvents.length;
+
+    // Try to set the same unread indicator value
+    instance.updateBotUnreadIndicatorVisibility(false);
+    instance.updateBotUnreadIndicatorVisibility(false);
+
+    // Should not have fired additional events for unchanged state
+    expect(stateChangeEvents.length).toBe(initialCount);
+  });
+
+  it("STATE_CHANGE event state is frozen", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    let stateChangeEvent: any = null;
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvent = event;
+      },
+    });
+
+    // Trigger a state change
+    instance.updateBotUnreadIndicatorVisibility(true);
+
+    // Both previous and new states should be frozen
+    expect(Object.isFrozen(stateChangeEvent.previousState)).toBe(true);
+    expect(Object.isFrozen(stateChangeEvent.newState)).toBe(true);
   });
 });

carbon-design-system/carbon-ai-chat

packages/ai-chat/tests/instance/spec/getState_spec.ts
@@ -13,6 +13,8 @@ import {
   setupAfterEach,
   setupBeforeEach,
 } from "../helpers/chatInstanceTestHelpers";
+import { BusEventType } from "../../../src/types/events/eventBusTypes";
+import { ViewType } from "../../../src/types/instance/apiTypes";
 
 describe("ChatInstance.getState", () => {
   beforeEach(setupBeforeEach);
@@ -54,5 +56,93 @@ describe("ChatInstance.getState", () => {
     expect(() => {
       (state.humanAgent as { isConnecting: boolean }).isConnecting = true;
     }).toThrow();
+  });
+
+  it("fires STATE_CHANGE event when state changes", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    const stateChangeEvents: any[] = [];
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvents.push(event);
+      },
+    });
+
+    // Change the view state which should trigger STATE_CHANGE
+    await instance.changeView("mainWindow" as ViewType);
+
+    // Should have received at least one STATE_CHANGE event
+    expect(stateChangeEvents.length).toBeGreaterThan(0);
+
+    const lastEvent = stateChangeEvents[stateChangeEvents.length - 1];
+    expect(lastEvent.type).toBe(BusEventType.STATE_CHANGE);
+    expect(lastEvent.previousState).toBeDefined();
+    expect(lastEvent.newState).toBeDefined();
+    expect(lastEvent.newState.viewState.mainWindow).toBe(true);
+  });
+
+  it("provides both previous and new state in STATE_CHANGE event", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    let stateChangeEvent: any = null;
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvent = event;
+      },
+    });
+
+    // Change the unread indicator
+    instance.updateAssistantUnreadIndicatorVisibility(true);
+
+    // Should have received the event
+    expect(stateChangeEvent).not.toBeNull();
+    expect(stateChangeEvent.previousState.showUnreadIndicator).toBe(false);
+    expect(stateChangeEvent.newState.showUnreadIndicator).toBe(true);
+  });
+
+  it("does not fire STATE_CHANGE event when state does not change", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    const stateChangeEvents: any[] = [];
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvents.push(event);
+      },
+    });
+
+    const initialCount = stateChangeEvents.length;
+
+    // Try to set the same unread indicator value
+    instance.updateAssistantUnreadIndicatorVisibility(false);
+    instance.updateAssistantUnreadIndicatorVisibility(false);
+
+    // Should not have fired additional events for unchanged state
+    expect(stateChangeEvents.length).toBe(initialCount);
+  });
+
+  it("STATE_CHANGE event state is frozen", async () => {
+    const config = createBaseConfig();
+    const { instance } = await renderChatAndGetInstanceWithStore(config);
+
+    let stateChangeEvent: any = null;
+    instance.on({
+      type: BusEventType.STATE_CHANGE,
+      handler: (event) => {
+        stateChangeEvent = event;
+      },
+    });
+
+    // Trigger a state change
+    instance.updateAssistantUnreadIndicatorVisibility(true);
+
+    // Both previous and new states should be frozen
+    expect(Object.isFrozen(stateChangeEvent.previousState)).toBe(true);
+    expect(Object.isFrozen(stateChangeEvent.newState)).toBe(true);
   });
 });