585bc81178174a72961bb80f7ea48afea9fbd929
Ethan Winters <ethan.winters@gmail.com>
2025-12-12 11:35:30 -0500

    fix: header labels
    we lost the labels on the header buttons when moving to web components

--- packages/ai-chat/src/chat/components-legacy/header/Header.tsx
+++ packages/ai-chat/src/chat/components-legacy/header/Header.tsx
@@ -235,52 +235,22 @@ function Header(props: HeaderProps, ref: Ref<HasRequestFocus>) {
   let closeIsReversible = true;
   switch (minimizeButtonIconType) {
     case MinimizeButtonIconType.CLOSE:
-      closeIcon = (
-        <CloseLarge
-          aria-label={closeButtonLabel}
-          slot="icon"
-          className="cds-aichat--icon__close"
-        />
-      );
+      closeIcon = <CloseLarge aria-label={closeButtonLabel} slot="icon" />;
       break;
     case MinimizeButtonIconType.MINIMIZE:
-      closeIcon = (
-        <SubtractLarge
-          aria-label={closeButtonLabel}
-          slot="icon"
-          className="cds-aichat--icon__subtract"
-        />
-      );
+      closeIcon = <SubtractLarge aria-label={closeButtonLabel} slot="icon" />;
       break;
     case MinimizeButtonIconType.SIDE_PANEL_LEFT:
       closeIsReversible = false;
-      closeIcon = (
-        <SidePanelClose
-          aria-label={closeButtonLabel}
-          slot="icon"
-          className="cds-aichat--icon__side-panel-close"
-        />
-      );
+      closeIcon = <SidePanelClose aria-label={closeButtonLabel} slot="icon" />;
       break;
     case MinimizeButtonIconType.SIDE_PANEL_RIGHT:
       closeIsReversible = false;
       closeReverseIcon = true;
-      closeIcon = (
-        <SidePanelClose
-          aria-label={closeButtonLabel}
-          slot="icon"
-          className="cds-aichat--icon__side-panel-close"
-        />
-      );
+      closeIcon = <SidePanelClose aria-label={closeButtonLabel} slot="icon" />;
       break;
     default: {
-      closeIcon = (
-        <SubtractLarge
-          aria-label={closeButtonLabel}
-          slot="icon"
-          className="cds-aichat--icon__subtract"
-        />
-      );
+      closeIcon = <SubtractLarge aria-label={closeButtonLabel} slot="icon" />;
       break;
     }
   }
@@ -425,7 +395,7 @@ function Header(props: HeaderProps, ref: Ref<HasRequestFocus>) {
                 "cds-aichat--reverse-icon": closeReverseIcon,
               })}
               isReversible={closeIsReversible}
-              label={closeButtonLabel}
+              label={closeButtonLabel ?? ""}
               onClick={async () => {
                 onClickClose();
               }}
@@ -499,6 +469,7 @@ function HeaderButton({
   isReversible = true,
   tooltipPosition,
   testId,
+  label,
   disabled = false,
 }: HeaderButtonProps) {
   const buttonKindVal = buttonKind || BUTTON_KIND.GHOST;
@@ -514,6 +485,7 @@ function HeaderButton({
       tooltipPosition={tooltipPosition}
       data-testid={testId}
       disabled={disabled}
+      tooltipText={label}
     >
       {children}
     </Button>
