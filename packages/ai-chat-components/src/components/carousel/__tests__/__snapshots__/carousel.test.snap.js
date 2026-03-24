1dfdbf06789a85320f5ddeae5159c6e3e32ee142
David Menendez <dmenend@us.ibm.com>
2026-03-24 14:23:20 -0500

    fix: tests

--- packages/ai-chat-components/src/components/carousel/__tests__/__snapshots__/carousel.test.snap.js
+++ packages/ai-chat-components/src/components/carousel/__tests__/__snapshots__/carousel.test.snap.js
@@ -12,17 +12,28 @@ snapshots["carousel should render with cds-aichat-carousel minimum attributes"] 
         slide 1
       </div>
       <div
+        aria-hidden="true"
         class="carousel__view"
         data-index="1"
+        inert=""
       >
         slide 2
       </div>
       <div
+        aria-hidden="true"
         class="carousel__view"
         data-index="2"
+        inert=""
       >
         slide 3
       </div>
+    </div>
+    <div
+      aria-atomic="true"
+      aria-live="polite"
+      class="carousel__live-region"
+    >
+      Item 1 of 3
     </div>
   </div>
 </cds-aichat-carousel>
