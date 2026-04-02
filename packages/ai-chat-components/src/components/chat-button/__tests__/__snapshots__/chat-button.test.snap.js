843faf6881ed341930d543a375c463ea003efd85
Warren Blood <69060697+warrenmblood@users.noreply.github.com>
2026-04-02 15:52:08 -0400

    fix: link launch icon alignment (#1077)

--- packages/ai-chat-components/src/components/chat-button/__tests__/__snapshots__/chat-button.test.snap.js
+++ packages/ai-chat-components/src/components/chat-button/__tests__/__snapshots__/chat-button.test.snap.js
@@ -3,21 +3,7 @@ export const snapshots = {};
 
 snapshots["chat-button should render with cds-aichat-button minimum attributes"] = 
 `<cds-aichat-button
-  has-main-content=""
-  kind="primary"
-  size="lg"
-  tab-index="0"
-  tooltip-alignment=""
-  tooltip-position="top"
-  type="button"
->
-  button
-</cds-aichat-button>
-`;
-/* end snapshot chat-button should render with cds-aichat-button minimum attributes */
-
-snapshots["chat-button should render with cds-aichat-button minimum attributes"] = 
-`<cds-aichat-button
+  danger-description="danger"
   has-main-content=""
   kind="primary"
   size="lg"
