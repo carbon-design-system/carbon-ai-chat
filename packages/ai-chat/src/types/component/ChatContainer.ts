2d6c1725327e6e1152c54a56144607da90bab8a4
Ethan Winters <ethan.winters@gmail.com>
2026-04-29 10:32:15 -0400

    feat: typeahead/mentions/commands (#1211)
    Co-authored-by: Warren Blood <69060697+warrenmblood@users.noreply.github.com>

--- packages/ai-chat/src/types/component/ChatContainer.ts
+++ packages/ai-chat/src/types/component/ChatContainer.ts
@@ -7,7 +7,7 @@
  *  @license
  */
 
-import { type HTMLAttributes, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { type ChatInstance, WriteableElements } from "../instance/ChatInstance";
 import { GenericItem, Message, MessageResponse } from "../messaging/Messages";
 import { PublicConfig } from "../config/PublicConfig";
@@ -110,8 +110,7 @@ type RenderWriteableElementResponse = {
  *
  * @category React
  */
-interface ChatContainerProps
-  extends PublicConfig, Omit<HTMLAttributes<HTMLElement>, keyof PublicConfig> {
+interface ChatContainerProps extends PublicConfig {
   /**
    * This function is called before the render function of Carbon AI Chat is called. This function can return a Promise
    * which will cause Carbon AI Chat to wait for it before rendering.
