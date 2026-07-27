b2da1bc0f84fc640167b3813be53b53511895228
Alexander Melo <Alexander.Melo@ibm.com>
2026-07-27 10:07:37 -0400

    feat: adds launcher.stories.js

--- packages/ai-chat-components/src/components/launcher/__stories__/launcher.stories.js
+++ packages/ai-chat-components/src/components/launcher/__stories__/launcher.stories.js
@@ -1,10 +1,10 @@
-/**
- * @license
+/*
+ *  Copyright IBM Corp. 2025, 2026
  *
- * Copyright IBM Corp. 2025, 2026
+ *  This source code is licensed under the Apache-2.0 license found in the
+ *  LICENSE file in the root directory of this source tree.
  *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
+ *  @license
  */
 
 import "../src/launcher";
@@ -15,6 +15,18 @@ import { ifDefined } from "lit/directives/if-defined.js";
 export default {
   title: "Components/Launcher",
   component: "cds-aichat-launcher",
+};
+
+const argTypes = {
+  launcherHidden: { control: "boolean" },
+  showUnreadIndicator: { control: "boolean" },
+  unreadMessageCount: { control: "number" },
+  closedLabel: { control: "text" },
+  openLabel: { control: "text" },
+  aiEnabled: { control: "boolean" },
+  launcherAvatarUrl: { control: "text" },
+  unreadLabel: { control: "text" },
+  onToggle: { table: { disable: true } },
 };
 
 const Template = (args) => html`
@@ -41,6 +53,7 @@ export const Default = {
     aiEnabled: false,
     onToggle: fn(),
   },
+  argTypes,
   render: Template,
 };
 
@@ -52,6 +65,7 @@ export const AIEnabled = {
     closedLabel: "Open AI chat",
     openLabel: "Close AI chat",
   },
+  argTypes,
   render: Template,
 };
 
@@ -62,5 +76,17 @@ export const WithUnreadCount = {
     unreadMessageCount: 3,
     unreadLabel: "3 unread messages",
   },
+  argTypes,
+  render: Template,
+};
+
+export const WithAvatar = {
+  name: "With avatar",
+  args: {
+    ...Default.args,
+    launcherAvatarUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/240px-User-avatar.svg.png",
+  },
+  argTypes,
   render: Template,
 };
